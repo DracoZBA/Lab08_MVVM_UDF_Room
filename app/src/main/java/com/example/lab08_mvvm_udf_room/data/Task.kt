24659bca279f8d906b5694fd16a6a71897c39a97
DracoZBA <yoaldair27@gmail.com>
2025-05-15 19:16:50 -0500

    "Creación de la entidad Task con Room"

--- app/src/main/java/com/example/lab08_mvvm_udf_room/data/Task.kt
+++ app/src/main/java/com/example/lab08_mvvm_udf_room/data/Task.kt
@@ -1,4 +1,23 @@
 package com.example.lab08_mvvm_udf_room.data
 
-class Task {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val description: String,
+    val isCompleted: Boolean = false
+)
+
+
+
+
+
+
+
+
+
+
+
+
