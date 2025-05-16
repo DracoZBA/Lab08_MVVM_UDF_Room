d0a77ebcb406fdf339d45c51fcb7507fb05f61e8
DracoZBA <yoaldair27@gmail.com>
2025-05-15 19:18:41 -0500

    "Creación de la interfaz DAO con operaciones básicas"

---
+++ app/src/main/java/com/example/lab08_mvvm_udf_room/data/TaskDao.kt
@@ -0,0 +1,21 @@
+package com.example.lab08_mvvm_udf_room.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM tasks")
+    suspend fun getAllTasks(): List<Task>
+
+    @Insert
+    suspend fun insertTask(task: Task)
+
+    @Update
+    suspend fun updateTask(task: Task)
+
+    @Query("DELETE FROM tasks")
+    suspend fun deleteAllTasks()
+}
---
+++ app/src/main/java/com/example/lab08_mvvm_udf_room/data/TaskDatabase.kt
@@ -0,0 +1,9 @@
+package com.example.lab08_mvvm_udf_room.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+}
